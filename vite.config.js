02b14d5084be127992a58ee91f8fb05a910a181b
richie1988 <richiessikaonga@gmail.com>
2024-09-12 19:33:36 +0200

    Layout implementation

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // Add this line
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/icons-material'] // Exclude this from bundling
+      external: ['@mui/icons-material'], // Exclude this from bundling
     }
   },
   resolve: {
